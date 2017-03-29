0ddef1c188921aeef4ad933080418d879a876634
Liam Norton <liam.norton@flippa.com>
2017-03-30 00:42:37 +1100

    New docker & compose

--- provision.sh
+++ provision.sh
@@ -1,32 +1,31 @@
 #!/bin/bash -ex
 
 sudo \
-  DOCKER_VER="1.10.2" \
-  COMPOSE_VER="1.6.2" \
+  DOCKER_VER="1.12.3" \
+  DOCKER_URL="https://apt.dockerproject.org/repo" \
+  COMPOSE_VER="1.9.0" \
   DOCKER_USER=`whoami` \
   sh <<'EOF'
+
 apt-key adv \
   --keyserver hkp://p80.pool.sks-keyservers.net:80 \
   --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' \
-  > /etc/apt/sources.list.d/docker.list
+echo "deb $DOCKER_URL ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 
 DEBIAN_FRONTEND=noninteractive \
   apt-get -y update && \
-  apt-get -y upgrade && \
-  apt-get -y --force-yes --no-install-recommends install \
-    ack-grep \
-    docker-engine=${DOCKER_VER}-0~trusty \
-    git-core \
-    linux-image-extra-$(uname -r) \
-    make \
-    unzip \
-    vim
+  apt-get -y upgrade
 
-dpkg-divert --local --divert /usr/bin/ack --rename --add /usr/bin/ack-grep
+apt-get -y --force-yes install \
+  docker-engine=${DOCKER_VER}-0~trusty \
+  linux-image-extra-$(uname -r) \
+  git make unzip vim
 
-echo "# aws cli"
+dpkg-divert \
+  --local --divert /usr/bin/ack \
+  --rename --add /usr/bin/ack-grep
+
 cd /tmp
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
@@ -34,17 +33,14 @@ apt-get -y purge unzip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 rm -rf awscli-bundle*
 
-echo "# docker-compose"
 compose_url="https://github.com/docker/compose/releases/download"
 compose_pkg="docker-compose-`uname -s`-`uname -m`"
 curl -sSL "$compose_url/$COMPOSE_VER/$compose_pkg" > docker-compose
 mv docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-echo "# docker user groups"
 usermod -aG docker ${DOCKER_USER}
 usermod -aG docker $(whoami)
-
 echo dev-vm > /etc/hostname
 EOF
 
